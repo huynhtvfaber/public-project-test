3ed017e3450424d684cfb54a5a9dde2c47c02e90
Huynh Tan Vy <huynhtanvy@fabercompany.co.jp>
2020-01-02 10:06:35 +0700

    init project test asciidoctor

---
+++ src/main/java/com/amazonelinux/asciidoctor/Main.java
@@ -0,0 +1,12 @@
+package com.amazonelinux.asciidoctor;
+
+/**
+ *
+ * @author Huynh Tan Vy
+ * @email huynhtanvy@fabercompany.co.jp
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("12312312321");
+    }
+}
